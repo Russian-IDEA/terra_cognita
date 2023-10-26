6146ac1cbb80002a7476a4ff713dd5f15827683a
Andrey <andreydem42@gmail.com>
2023-10-26 15:14:43 +0300

    first commit

---
+++ terra_cognita/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for terra_cognita project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'terra_cognita.settings')
+
+application = get_wsgi_application()
