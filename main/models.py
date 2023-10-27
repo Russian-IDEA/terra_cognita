2d81fd6cf4a8846004c34580997311e6deb1c2fa
Andrey <andreydem42@gmail.com>
2023-10-27 16:15:23 +0300

    make profile page

--- main/models.py
+++ main/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    name = models.CharField('Название', max_length=20)
+    x1 = models.FloatField()
+    y1 = models.FloatField()
+    x2 = models.FloatField()
+    y2 = models.FloatField()
+    date = models.DateTimeField('Время исполнения')
+    is_cancelled = models.BooleanField('Отменён', default=False)
+
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from .models import User
+from .models import User, Order
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.contrib.auth.decorators import login_required
 from django.db.utils import IntegrityError
 import os
+import datetime
+from random import randint
 
 
 @ensure_csrf_cookie
@@ -32,7 +34,15 @@ def home(request):
 
 @login_required(login_url='/')
 def profile(request):
-    return render(request, "main/profile.html", {'user': request.user.username})
+    if request.method == 'POST':
+        name = request.POST['name']
+        points = list(map(float, request.POST['points'].split(',')))
+        date = datetime.datetime.now() + datetime.timedelta(seconds=randint(1_000, 1_000_000))
+        Order.objects.create(
+            user=request.user, name=name, x1=points[0], y1=points[1], x2=points[2], y2=points[3], date=date)
+    orders = Order.objects.all().order_by('id').reverse()
+    return render(request, "main/profile.html", {'user': request.user.username, 'orders': orders,
+                                                 "current_time": datetime.datetime.now()})
 
 
 def add(request):
